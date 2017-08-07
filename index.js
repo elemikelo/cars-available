3fb5adeb411951edd43904c05dd627af9493ecea
Luis Miguel Álvarez <elemikelo@gmail.com>
2017-08-07 13:36:53 +0200

    Refactor indexjs

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 
 const data = require('./data');
 
-function availability(numberTotalCars, arrayCarsDates, input) {
+function seeAvailability(numberTotalCars, arrayCarsDates, input) {
 
   var sameCarUsed = 0;
   var carsAvailable = 0;
 
-  // Don't exist rentals
+  /*  See if don't exist rentals */
   if (arrayCarsDates.length === 0) {
     console.log(`Cars Available ${numberTotalCars} 😎 `);
     return true;
   }
 
-  // String elements to number **  preparation time = 1h = 3600000
+
+  /* Converts start and end dates values in milliseconds in new array to compare. 
+     The preparation time was also added to the final date */
   let newArrayCarsDates = arrayCarsDates.map(date => {
     return {
       start: new Date(date.start).getTime(),
@@ -21,19 +23,33 @@ function availability(numberTotalCars, arrayCarsDates, input) {
     }
   });
 
-  // Sort array by start dates
-  newArrayCarsDates.sort(function (a, b) { return a.start - b.start });
+  /* Copy array and order by the end of dates to see if the date of entry 
+  is greater than the final rental date */
+  let copyArray = newArrayCarsDates.map(dates => dates);
+  let orderByDatesEnd = copyArray.sort((a, b) => a.end + b.end);
+  if (orderByDatesEnd[0].end < input.start) {
+    console.log(`Cars Available ${numberTotalCars} 😎 `);
+    return true;
+  }
 
-  // Return array rentals with input  
+  /* Sorts rentals by start dates to calculate */
+  newArrayCarsDates.sort((a, b) => a.start - b.start);
+
+  /* Returns a new filtered array for rents contained of the date input */
   let filterRents = newArrayCarsDates.filter(date => {
     if (date.start >= input.start && (date.end - data.preparationTime) <= input.end) {
       return date;
     }
   });
 
-  // Calculates how many cars you need for existing rentals
-  let calculeCarUsed = filterRents.reduce((a, b) => {
-    if (!compareRangesDates(a, b)) {
+  if (filterRents.length == 0) {
+    filterRents = newArrayCarsDates;
+  }
+
+
+  /* Calculate how many same cars are used for different  rents in the total  rentals array */
+  let calculateSameCars = filterRents.reduce((a, b) => {
+    if (!compareIntervals(a, b)) {
       sameCarUsed += 1;
       return b;
     }
@@ -43,8 +59,9 @@ function availability(numberTotalCars, arrayCarsDates, input) {
   carsAvailable = (numberTotalCars - filterRents.length) + sameCarUsed;
 
   // Compare input with the number of cars in stock
-  let arrayReservedDates = newArrayCarsDates.map(date => {
-    if (!compareRangesDates(input, date)) {
+
+  let calculateStockCars = newArrayCarsDates.map(date => {
+    if (!compareIntervals(input, date)) {
       carsAvailable--;
     }
   });
@@ -58,17 +75,19 @@ function availability(numberTotalCars, arrayCarsDates, input) {
   }
 }
 
+/* add new rental into array */
 function addRental(input, arrayCarsDates) {
 
   return arrayCarsDates.concat(input)
 
 }
-
-function inputDatesConverted(input) {
+/* convert "string"  input values to numbers (Milliseconds) */
+function convertToNumbers(input) {
   return { start: new Date(input.start).getTime(), end: new Date(input.end).getTime() }
 }
 
-function compareRangesDates(rangeOne, rangeTwo) {
+/* verify if date intervals are contained */
+function compareIntervals(rangeOne, rangeTwo) {
 
   if (rangeOne.end > rangeTwo.start && rangeOne.start < rangeTwo.end) {
     // need new car
@@ -79,13 +98,14 @@ function compareRangesDates(rangeOne, rangeTwo) {
   }
 }
 
+/* create rentals according cars available */
 function createRent(input, arrayCarsDates, numberTotalCars) {
 
   if (input.end < input.start) {
     return console.log('Date Invalid ⛔️ ');
   }
 
-  if (availability(numberTotalCars, arrayCarsDates, input)) {
+  if (seeAvailability(numberTotalCars, arrayCarsDates, input)) {
 
     addRental(input, arrayCarsDates);
 
@@ -96,5 +116,5 @@ function createRent(input, arrayCarsDates, numberTotalCars) {
   }
 }
 
-createRent(inputDatesConverted(data.inputRent), data.rentedCars, data.cars);
+createRent(convertToNumbers(data.inputRent), data.rentedCars, data.cars);
 
